f8a21e557b49e10a76b44c6206fe5e675b9f5e8b
Macbook Air <AllexSantos@MacBook-Air-de-Alexsandro.local>
2022-06-01 20:09:34 -0300

    django: Adicionar urls de frontend em core

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', include('frontend.urls')),
     path('cotacao/', include('cotacao.urls')),
 ]
