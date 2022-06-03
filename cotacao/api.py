0b0752b0a1130159c34fbf51ec14e9fc40ee793d
Macbook Air <AllexSantos@MacBook-Air-de-Alexsandro.local>
2022-06-03 19:35:20 -0300

    api: Checar validade dos dados com serializer

--- cotacao/api.py
+++ cotacao/api.py
@@ -4,10 +4,15 @@ from rest_framework.views import APIView
 from cotacao.use_cases import GetRateUseCase
 from cotacao.adapters.gateway import VATcomplyRateGateway
 from cotacao.adapters.operator import RequestsHttpOperator
+from cotacao.serializers import GetRateSerializer
 
 
 class GetRateView(APIView):
     def get(self, request):
+        serializer = GetRateSerializer(data=request.query_params)
+        if not serializer.is_valid():
+            return Response(serializer.errors, 400)
+
         try:
             use_case = GetRateUseCase(
                 gateway=VATcomplyRateGateway,
