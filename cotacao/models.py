d92618669545b70166527fd2761bd79adf8b3ced
Macbook Air <AllexSantos@MacBook-Air-de-Alexsandro.local>
2022-06-04 00:57:26 -0300

    models: Criar representações das tabelas no banco

--- cotacao/models.py
+++ cotacao/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    abbreviation = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.abbreviation
+
+
+class Rate(models.Model):
+    currency = models.ForeignKey(
+        'Currency', on_delete=models.CASCADE, related_name='rates')
+    amount = models.CharField(max_length=20)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.currency} {self.amount} {self.date}'
