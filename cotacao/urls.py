1589ac920bc639a1fcf5157405ae7f68a35e4d1f
Macbook Air <AllexSantos@MacBook-Air-de-Alexsandro.local>
2022-06-04 01:04:39 -0300

    urls: Registrar entradas p endpoints de consulta

--- cotacao/urls.py
+++ cotacao/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from cotacao.api import (
     GetRateView,
+    CurrencyViewSet,
+    RateViewSet,
 )
 
 
+router = DefaultRouter()
+router.register('currencies', CurrencyViewSet, basename='cotacao-currency')
+router.register('rates', RateViewSet, basename='cotacao-rate')
+
 urlpatterns = [
     path('get-rate/', GetRateView.as_view(), name='cotacao-get-rate'),
+    path('', include(router.urls)),
 ]
