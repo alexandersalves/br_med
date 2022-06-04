e4f8ce482c9ea2bfebac8f4ee17d86042221f9a6
Macbook Air <AllexSantos@MacBook-Air-de-Alexsandro.local>
2022-06-04 01:57:00 -0300

    settings: Configurar variáveis de ambiente

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import dj_database_url
+import environ
 
+
+# Initialise environment variables
+get_env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = get_env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = get_env('DEBUG').upper() == 'TRUE'
 
 ALLOWED_HOSTS = []
 
