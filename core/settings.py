f86023333a4197c0ddef6a16020eb7a49285bca8
Macbook Air <AllexSantos@MacBook-Air-de-Alexsandro.local>
2022-06-04 01:29:57 -0300

    settings: Configurar banco de dados para deploy

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,6 +96,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+if not DEBUG:
+    # Parse database configuration from $DATABASE_URL
+    DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
